b00a40ca7ac6a0bdb3dea9270035c154d653db36
Tkm1Hub <>
2025-10-11 12:22:00 +0900

    EnemyManagerクラスの追加

--- GameBase/EnemyManager.h
+++ GameBase/EnemyManager.h
@@ -1,1 +1,17 @@
 #pragma once
+
+class Character;
+class EnemyManager
+{
+public:
+	void AddEnemy(const VECTOR& pos);
+
+	void Init();
+	void Load();
+	void Update();
+	void Draw();
+
+private:
+	static constexpr int ENEMY_NUM = 10;
+	std::vector<std::shared_ptr<Character>> enemys;
+};
