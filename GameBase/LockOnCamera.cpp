b71fa1c4b4e84aa57e28c8ffe767b3a27acc3d37
Tkm1Hub <takumis_17214@icloud.com>
2025-10-27 19:40:27 +0900

    ロックオンカメラの作成

--- GameBase/LockOnCamera.cpp
+++ GameBase/LockOnCamera.cpp
@@ -1,3 +1,41 @@
 #include "stdafx.h"
 #include "LockOnCamera.h"
+#include "Player.h"
+#include "EnemyManager.h"
 
+void LockOnCamera::Update()
+{
+	target = CulcTargetPos();
+}
+
+// 注視点を計算
+VECTOR LockOnCamera::CulcTargetPos()
+{
+	// プレイヤーと敵の座標を取得
+	VECTOR playerPos = m_pPlayer->GetPosition();
+	VECTOR enemyPos = EnemyManager::GetEnemyManager().GetNearestEnemyPos(playerPos);
+
+	// 中点の座標
+	VECTOR midPos = VScale(VAdd(playerPos, enemyPos), 0.5f);
+
+	return midPos;
+}
+
+// カメラの座標を計算
+VECTOR LockOnCamera::CulcCameraPos()
+{
+	// プレイヤーと敵の座標を取得
+	VECTOR playerPos = m_pPlayer->GetPosition();
+	VECTOR enemyPos = EnemyManager::GetEnemyManager().GetNearestEnemyPos(playerPos);
+
+	// 敵とプレイヤーの距離を計算
+	VECTOR diff = VSub(playerPos, enemyPos);
+
+	// 敵からプレイヤーへの方向ベクトル
+	VECTOR dir = VNorm(diff);
+
+	// カメラの座標（playerPos + dir * dist）
+	VECTOR CameraPos = VAdd(playerPos, VScale(dir, CameraDist));
+
+	return CameraPos;
+}
--- GameBase/LockOnCamera.h
+++ GameBase/LockOnCamera.h
@@ -1,16 +1,24 @@
 #pragma once
 #include "VirtualCameraBase.h"
 class Player;
+class EnemyBase;
 class LockOnCamera :public VirtualCameraBase
 {
 public:
 	LockOnCamera(const std::string):VirtualCameraBase(name) {}
 
-	void SetPlayer(const std::weak_ptr<Player>& playerPtr);
+	void SetPlayer(const std::shared_ptr<Player>& playerPtr);
 
 	void Init();
 	void Update() override;
 
 private:
-	std::weak_ptr<Player> player;
+	static constexpr float CameraDist = 100.0f;
+	static constexpr float CameraHeight = 70.0f;
+
+	std::shared_ptr<Player> m_pPlayer;
+
+	VECTOR CulcTargetPos();
+	VECTOR CulcCameraPos();
+
 };
